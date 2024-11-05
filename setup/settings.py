314349d3796bbcd1cbef73d066ac96cb51083c1a
Pablo Troli <brawlixo123@gmail.com>
2024-11-05 19:24:23 -0300

    🔧 Add Configuration files

--- setup/settings.py
+++ setup/settings.py
@@ -22,10 +22,20 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Apps tercerizadas
+    "rest_framework",
+    "corsheaders",
     "django_extensions",
     # Minhas apps
     "snippets.apps.SnippetsConfig",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
